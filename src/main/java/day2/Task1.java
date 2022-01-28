71822ca6268a63fba342adcd7ddafd39d3403efe
Sia Oleinik <oleskokh@gmail.com>
2022-01-28 15:19:32 -0500

    add days 2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи количество этажей");
         int levels = scanner.nextInt();
         if(levels<1){
             System.out.println("Ошибка ввода");
