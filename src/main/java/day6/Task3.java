394a42736ec412994bf8d89f988a116316088951
Sia Oleinik <oleskokh@gmail.com>
2022-01-30 21:26:40 -0500

    add day 6

--- src/main/java/day6/Task3.java
+++ src/main/java/day6/Task3.java
@@ -2,8 +2,8 @@ package day6;
 
 public class Task3 {
     public static void main(String[] args) {
-        Student student1 = new Student("Zuckerberg");
+        Student student1 = new Student("Zucerberg");
         Teacher teacher1 = new Teacher("Gates", "Computer Science");
-        teacher1.evaluate("Zuckerberg");
+        teacher1.evaluate("Zucerberg");
     }
 }
