6a8ce9665230bf9236aa46e8e914f17382bcc093
Sia Oleinik <oleskokh@gmail.com>
2022-02-13 20:20:55 -0500

    complete day 9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -3,5 +3,16 @@ package day9.Task1;
 public class Task1 {
     public static void main(String[] args) {
 
+        Student student = new Student("Sasha", "582");
+        Teacher teacher = new Teacher("Jack", "Philosophy");
+
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+
+        student.printInfo();
+        teacher.printInfo();
+
     }
+
+
 }
